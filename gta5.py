baccf7252cf65c84555de8655cc2b5815273d0bc
Raghav Gupta <E19CSE258@bennett.edu.in>
2021-09-29 23:53:40 +0530

    key input done

--- gta5.py
+++ gta5.py
@@ -1,11 +1,15 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
-
+import time
+from directkeys import PressKey,W,A,S,D
+time.sleep(5)
 while(True):
-    printscreen_pil =  ImageGrab.grab(bbox=(0,40,980,510))
+    PressKey(W)
+    time.sleep(1)
+    printscreen_pil =  ImageGrab.grab()
     # printscreen_numpy =   np.array(printscreen_pil.getdata(),dtype='uint8')\
-    # .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3)) 
+    # .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
     cv2.imshow('window',np.array(printscreen_pil))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
